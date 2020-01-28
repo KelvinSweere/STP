27f811b761676b17ca8c8a7793dc7cd9a454080f
Neki Dzemaili <neki.dzemaili@gmail.com>
2020-01-28 14:41:09 +0100

    Add extended support for changing cuttof frequency

--- STP_convolution_filter/libs/keys_int.c
+++ STP_convolution_filter/libs/keys_int.c
@@ -25,7 +25,6 @@ G_KEY is a global variable which holds the last pressed button
 */
 
 #include "main.h"
-#if KEYS_INT
 
 /****************Globals********************************/
 int G_KEY;				// Global variable. G_KEY is filled with the pressed key
@@ -92,129 +91,60 @@ void KEYS_INT_init(void)
 volatile int i;
 volatile float incrementor;
 
-void EXTI0_IRQHandler(void)
 /* External Interrupt0 Interrupt Request Handler
  * This function is active after a button on the keyboard is pressed
  * This function puts the pressed key in a global variable
  * and can be used to run code with the pressed button
  */
+void EXTI0_IRQHandler(void)
 {
 	if(EXTI_GetITStatus(EXTI_Line0) != RESET)
 	{
-		__asm("CPSID   I"); // disable interrupts
+		__asm("CPSID   I"); 			/* disable interrupts 								*/
 
-		GPIO_ResetBits(GPIOD, KEY_ROW);	// Reset all rows so KEYS_read can start reading
-		G_KEY = KEYS_read();			// Read the pressed key
+		GPIO_ResetBits(GPIOD, KEY_ROW);	/* Reset all rows so KEYS_read can start reading	*/
+		G_KEY = KEYS_read();			/* Read the pressed key								*/
 
-		//fc zit tussen 2kHz en 18kHz
+		/* Check which key has been pressed */
 		switch(G_KEY)
 		{
-			case  1: fc += 0.0025; break;		// Verandering van +100   Hz
-			case  2: fc += 0.025;  break;		// Verandering van +1000  Hz
+			case  1: fc += (float)(1.0    / F_SAMPLE); break;	/* Verandering van +1    Hz */
+			case  2: fc += (float)(10.0   / F_SAMPLE); break;	/* Verandering van +10   Hz */
+			case  3: fc += (float)(100.0  / F_SAMPLE); break;	/* Verandering van +100  Hz */
+			case  4: fc += (float)(1000.0 / F_SAMPLE); break;	/* Verandering van +1000 Hz */
 
-			case  5: fc -= 0.0025; break;		// Verandering van -100   Hz
-			case  6: fc -= 0.025;  break;		// Verandering van -1000  Hz
+			case  5: fc -= (float)(1.0    / F_SAMPLE); break;	/* Verandering van -1    Hz */
+			case  6: fc -= (float)(10.0   / F_SAMPLE); break;	/* Verandering van -10   Hz */
+			case  7: fc -= (float)(100.0  / F_SAMPLE); break;	/* Verandering van -100  Hz */
+			case  8: fc -= (float)(1000.0 / F_SAMPLE); break;	/* Verandering van -1000 Hz */
 
-			default:                 break;
+			default:               break;
 		}
 
-		//Zodat fc niet out of boundary komt
-		if(fc <= 0.05)
-			fc = 0.05;
-		else if(fc >= 0.375)
-			fc = 0.375;
+		/* Prevent fc value getting to high / to low */
+		if(fc <= 0.05)			fc = 0.05;
+		else if(fc >= 0.375)	fc = 0.375;
 
+		ConvPrintVal();			/* Print cuttof frequency on LCD */
+		ConvGenerateKernel();	/* Generate new kernel			 */
 
-		//Print fc van 0 tot 0.5
-		LCD_clear();
-		LCD_put("fc = ");
-
-		if(fc < 0.1)
-			LCD_put("0.0");
-		else
-			LCD_put("0.");
-
-		LCD_putint(((int)(fc*10000)));
-
-		//Print fc in Hz
-		LCD_XY(0,1);
-		LCD_put("fc = ");
-		LCD_putint(((int)(fc*F_SAMPLE)));
-		LCD_put(" Hz");
-
-		//bepaal_deler();	//bepaal welke deler nodig is voor het convolutiefilter
-		deler = (int)((1+fc) * H_MULTIPLIER);
-		UART_printf(256, "fc: %f \t deler: %d \r\n", fc, deler);
-		ConvGenerateKernel();
-
+		/* Clear buffer */
 		for(i=0; i<M; i++)
 			x[i] = 0;
 
-		GPIO_SetBits(GPIOD, KEY_ROW);	// Make the ROW high, so we can wait for an interrupt
-
-		EXTI_ClearITPendingBit(EXTI_Line0);	// Clear the interrupt bit
+		GPIO_SetBits(GPIOD, KEY_ROW);		/* Make the ROW high, so we can wait for an interrupt	*/
+		EXTI_ClearITPendingBit(EXTI_Line0);	/* Clear the interrupt bit								*/
 
 		__asm("CPSIE   I"); // enable interrupts
 	}
 }
 
-void bepaal_deler(void)
-{
-	//Bepaal de deler
-	if(fc >= 0.05 && fc <= 0.1125)			//0.06 verschil
-		deler = 100000;
-	else if(fc > 0.1125 && fc <= 0.1475)	//0.03 verschil
-		deler = 110000;
-	else if(fc > 0.1475 && fc <= 0.1775)
-		deler = 120000;
-	else if(fc > 0.1775 && fc <= 0.2075)
-		deler = 130000;
-	else if(fc > 0.2075 && fc <= 0.2225)	//0.015 verschil
-		deler = 140000;
-	else if(fc > 0.2225 && fc <= 0.2375)
-		deler = 150000;
-	else if(fc > 0.2375 && fc <= 0.2575)
-		deler = 160000;
-	else if(fc > 0.2575 && fc <= 0.2725)
-		deler = 170000;
-	else if(fc > 0.2725 && fc <= 0.2850)	//0.0125 verschil
-		deler = 180000;
-	else if(fc > 0.2850 && fc <= 0.2950)	//0.01 verschil
-		deler = 190000;
-	else if(fc > 0.2950 && fc <= 0.3050)
-		deler = 200000;
-	else if(fc > 0.3050 && fc <= 0.3150)
-		deler = 210000;
-	else if(fc > 0.3150 && fc <= 0.3225)	//0.0075 verschil
-		deler = 220000;
-	else if(fc > 0.3225 && fc <= 0.3300)
-		deler = 230000;
-	else if(fc > 0.3300 && fc <= 0.3375)
-		deler = 240000;
-	else if(fc > 0.3375 && fc <= 0.3450)
-		deler = 250000;
-	else if(fc > 0.3450 && fc <= 0.3525)
-		deler = 260000;
-	else if(fc > 0.3525 && fc <= 0.3575)	//0.005 verschil
-		deler = 270000;
-	else if(fc > 0.3575 && fc <= 0.3625)
-		deler = 280000;
-	else if(fc > 0.3625 && fc <= 0.3675)
-		deler = 290000;
-	else if(fc > 0.3675 && fc <= 0.3725)
-		deler = 300000;
-	else if(fc > 0.3725 && fc <= 0.3750)
-		deler = 310000;
-}
-
-
-
-unsigned int KEYS_read(void)
 /* Keys read
  * This function reads which key is pressed.
  * It does so by making a row high and check if there is a connection with a column.
  * If there is a connection the value is returned.
  */
+unsigned int KEYS_read(void)
 {
 	unsigned int key = 0;
 	GPIO_SetBits(PKEY_ROW, KEY_R4);	// Make row4 high
@@ -244,11 +174,11 @@ unsigned int KEYS_read(void)
 	return 0;
 }
 
-unsigned int KEYS_kolom(void)
 /* Keys Column
  * In this function the columns are checked if there is a connection.
  * If a column has a connection the return value is the column number.
  */
+unsigned int KEYS_kolom(void)
 {
 	unsigned int key = 0;	// Check if a row has a connection
 	if(GPIO_ReadInputDataBit(PKEY_COL, KEY_K1) == (uint8_t)Bit_SET)
@@ -261,7 +191,3 @@ unsigned int KEYS_kolom(void)
 		key = 4;
 	return key;
 }
-
-
-
-#endif
